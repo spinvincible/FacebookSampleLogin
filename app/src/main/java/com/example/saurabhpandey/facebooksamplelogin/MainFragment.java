9a02a3743a36fc810dc7a5c384a46e768cfa39a2
SaurabhPandey <saurabh.joy8@gmail.com>
2015-06-14 00:02:42 -0700

    Initial commit

---
+++ app/src/main/java/com/example/saurabhpandey/facebooksamplelogin/MainFragment.java
@@ -0,0 +1,31 @@
+package com.example.saurabhpandey.facebooksamplelogin;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.facebook.FacebookSdk;
+
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class MainFragment extends Fragment {
+
+    public MainFragment() {
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        FacebookSdk.sdkInitialize(getActivity().getApplicationContext());
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_main, container, false);
+    }
+}
