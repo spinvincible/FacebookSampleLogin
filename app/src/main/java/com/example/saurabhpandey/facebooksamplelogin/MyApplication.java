2c08a907dbdfc727fc1a4b3773b2ffd827d64073
SaurabhPandey <saurabh.joy8@gmail.com>
2015-06-17 07:31:34 -0700

    Final Commit

--- app/src/main/java/com/example/saurabhpandey/facebooksamplelogin/MyApplication.java
+++ app/src/main/java/com/example/saurabhpandey/facebooksamplelogin/MyApplication.java
@@ -30,7 +30,6 @@ public class MyApplication extends Application {
             for (Signature signature : info.signatures) {
                 MessageDigest md = MessageDigest.getInstance("SHA");
                 md.update(signature.toByteArray());
-                Log.d("KeyHash:", Base64.encodeToString(md.digest(), Base64.DEFAULT));
             }
         } catch (PackageManager.NameNotFoundException e) {
 
